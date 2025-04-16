3d9e686b7be0823989e04b2e3b5c82d7ec562ae5
ozzzxyn <osvaldoramirez113@gmail.com>
2025-04-15 20:29:10 -0700

    Progress
    Last commit of 4/15/2025

--- src/comp/home.js
+++ src/comp/home.js
@@ -1,13 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './home.css'
 import { Link } from 'react-router-dom';
 import HomeProduct from './home_product';
 import { FaRegEye } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
+import { RxInstagramLogo } from "react-icons/rx";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { IoShareSocial } from "react-icons/io5";
+
+
 
 
 const Home = () => {
+    //Product category
+    const [newProduct, setNewProduct] = useState([])
+    //Trending product
     const [trendingProduct, setTrendingProduct] = useState(HomeProduct)
+    // Filter trending product
+    const filtercate = (x) =>
+    {
+        const filterproduct = HomeProduct.filter((curElm) =>
+        {
+            return curElm.type === x 
+        })
+        setTrendingProduct(filterproduct)
+    }
+    //All Trending Product
+    const allTrendingProduct = () =>
+    {
+        setTrendingProduct(HomeProduct)
+    }
+
+    //Product Type
+    useEffect(() =>
+    {
+        productcategory()
+    })
+    const productcategory = () =>
+    {
+        const newcategory = HomeProduct.filter((x) => 
+        {
+            return x.type === 'new'
+        })
+        setNewProduct(newcategory)
+    }
     return (
     <>
     <div className='home'>
@@ -24,12 +61,12 @@ const Home = () => {
                 <div className='left_box'>
                     <div className='header'>
                         <div className='heading'>
-                            <h2>Trending Product</h2>
+                            <h2 onClick={() => allTrendingProduct ()}>Trending Product</h2>
                         </div>
                         <div className='cate'>
-                            <h3>New</h3>
-                            <h3>Featured</h3>
-                            <h3>Top Selling</h3>
+                            <h3 onClick={() => filtercate('new')}>New</h3>
+                            <h3 onClick={() => filtercate('featured')}>Featured</h3>
+                            <h3 onClick={() => filtercate('top')}>Top Selling</h3>
                         </div>
                     </div>
                     <div className='products'>
@@ -62,9 +99,104 @@ const Home = () => {
                                 })
                             }
                         </div>
+                        <button>Show More</button>
                     </div>
                 </div>
                 <div className='right_box'>
+                    <div className='right_container'>
+                        <div className='testimonial'>
+                            <div className='head'>
+                                <h3>Our Testimonial</h3>
+                            </div>
+                            <div className='detail'>
+                                <div className='img_box'>
+                                    <img src='image/' alt='testmonial'></img>
+                                </div>
+                                <div className='info'>
+                                    <h3>Chat Bot here maybe?</h3>
+                                    <h4>Web Designer</h4>
+                                    <p>blah blah blah description</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='newsletter'>
+                            <div className='head'>
+                                <h3>Newsletter</h3>
+                            </div>
+                            <div className='form'>
+                                <p>Join our Mailing list, unsure to keep</p>
+                                <input type='email' placeholder='E-mail' autoComplete='off'></input>
+                                <button>Subscribe</button>
+                                <div className='icon_box'>
+                                    <div className='icon'>
+                                        <RxInstagramLogo />
+                                    </div>
+                                    <div className='icon'>
+                                        <FaXTwitter />
+                                    </div>
+                                    <div className='icon'>
+                                        <FaYoutube />
+                                    </div>
+                                    <div className='icon'>
+                                        <IoShareSocial />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='banners'>
+            <div className='container'>
+                <div className='left_box'>
+                    <div className='box'>
+                        <img src='image/bannerplaceholder.jpg' alt='banner'></img>
+                    </div>
+                    <div className='box'>
+                        <img src='image/bannerplaceholder2.png' alt='banner'></img>
+                    </div>
+                </div>
+                <div className='right_box'>
+                    <div className='top'>
+                        <img src='image/bannerplaceholder3.png' alt =''></img>
+                        <img src='image/bannerplaceholder4.png' alt =''></img>
+                    </div>
+                    <div className='bottom'>
+                        <img src='image/bannerplaceholder5.webp' alt=''></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='product_type'>
+            <div className='container'>
+                <div className='box'>
+                    <div className='header'>
+                        <h2>New Product</h2>
+                    </div>
+                    {
+                        newProduct.map((curElm) =>
+                        {
+                            return(
+                                <>
+                                <div className='productbox'>
+                                    <div className='img-box'>
+                                        <img src={curElm.image} alt=''></img>
+                                    </div>
+                                    <div className='detail'>
+                                        <h3>{curElm.Name}</h3>
+                                        <p>$ {curElm.price}</p>
+                                        <div className='icon'>
+                                            <button><FaRegEye /></button>
+                                            <button><FaHeart /></button>
+                                            <button></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                </>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
