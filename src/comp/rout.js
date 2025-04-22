131f8668f8684070dde3b4ab409fccde4d712c6c
ozzzxyn <osvaldoramirez113@gmail.com>
2025-04-21 22:06:41 -0700

    Done

--- src/comp/rout.js
+++ src/comp/rout.js
@@ -3,7 +3,7 @@ import {Routes, Route} from 'react-router';
 import Home from './home';
 import Shop from './shop';
 import Cart from './cart';
-
+import Contact from './contact';
 
 const Rout = ({shop, Filter, allcatefilter, addtocart, cart, setCart}) => {
   return (
@@ -12,6 +12,7 @@ const Rout = ({shop, Filter, allcatefilter, addtocart, cart, setCart}) => {
         <Route path='/' element={<Home addtocart={addtocart}/>}/>
         <Route path='/collection' element={<Cart cart={cart} setCart={setCart}/>} />
         <Route path='Shop' element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} addtocart={addtocart}/>} />
+        <Route path='/contact' element={<Contact />} />
     </Routes>
     </>
   )
