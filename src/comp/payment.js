7cd27aff023241d8c589e591a8a4d38c9909dc04
Brian Ayala <brianayala@csu.fullerton.edu>
2025-05-02 16:56:49 -0700

    Added shipping and tax cost

--- src/comp/payment.js
+++ src/comp/payment.js
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom'
 const Payment = ({cart, setCart}) => {
 //Total price
 const total = cart.reduce((price, item) => price + item.qty * item.price, 0)
+const shipping = (total > 30) ? 0 : 20;
+const tax = total * 0.0725;
+const subtotal = (total + shipping + tax);
 
 
 const clearcart = () =>
@@ -63,7 +66,10 @@ const clearcart = () =>
             {
                 <>
                 <div className='Total'>
-                    <h4>Sub Total: ${total}</h4>
+                    <h4>Total: ${total.toFixed(2)}</h4>
+                    <h4>Shipping: {(shipping === 0) ? "Free!" : "$" + shipping.toFixed(2)}</h4>
+                    <h4>Taxes: ${tax.toFixed(2)}</h4>
+                    <h4>Sub-Total: ${subtotal.toFixed(2)}</h4>
                 </div>
                 <Link to='/thankyou'><button onClick={() => clearcart()}>Pay</button></Link>
                 </>
