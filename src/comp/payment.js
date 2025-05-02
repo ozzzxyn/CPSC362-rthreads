abf8a71817fce77c62b4e2d79c1069a317638608
Brian Ayala <brianayala@csu.fullerton.edu>
2025-05-02 16:10:13 -0700

    Cart clears after paying (don't ask how, it just works)

--- src/comp/payment.js
+++ src/comp/payment.js
@@ -6,9 +6,30 @@ import { FaWindowClose } from "react-icons/fa";
 
 
 
-const Payment = ({cart, setcart}) => {
+const Payment = ({cart, setCart}) => {
 //Total price
 const total = cart.reduce((price, item) => price + item.qty * item.price, 0)
+
+const removeproduct = (product) =>
+    {
+        const exist = cart.find((x) =>
+        {
+            return x.id === product.id
+        })
+        if(exist.qty > 0)
+        {
+            setCart(cart.filter((curElm) =>
+            {
+                return false
+            }))
+        }
+    }
+const clearcart = () =>
+    {
+        cart.map((curElm) => {
+            removeproduct(curElm)
+        })
+    }
 
   return (
     <>
@@ -60,7 +81,7 @@ const total = cart.reduce((price, item) => price + item.qty * item.price, 0)
                 <div className='Total'>
                     <h4>Sub Total: ${total}</h4>
                 </div>
-                <Link to='/thankyou'><button>Pay</button></Link>
+                <Link to='/thankyou'><button onClick={() => clearcart()}>Pay</button></Link>
                 </>
             }
         </div>
