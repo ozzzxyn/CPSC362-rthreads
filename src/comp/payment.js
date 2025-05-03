b24c40947f1c3f21615e7c5a76ebbf6a34c21f7d
Brian Ayala <brianayala@csu.fullerton.edu>
2025-05-02 17:19:42 -0700

    Swapped Total and subtotal

--- src/comp/payment.js
+++ src/comp/payment.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 
 const Payment = ({cart, setCart}) => {
 //Total price
-const total = cart.reduce((price, item) => price + item.qty * item.price, 0)
+const subtotal = cart.reduce((price, item) => price + item.qty * item.price, 0)
 const shipping = (total > 30) ? 0 : 20;
-const tax = total * 0.0725;
-const subtotal = (total + shipping + tax);
+const tax = subtotal * 0.0725;
+const total = (subtotal + shipping + tax);
 
 
 const clearcart = () =>
@@ -82,10 +82,10 @@ const clearcart = () =>
                 cart.length > 0 &&
                 <>
                 <div className='Total'>
-                    <h4>Total: ${total.toFixed(2)}</h4>
+                    <h4>Sub-Total: ${subtotal.toFixed(2)}</h4>
                     <h4>Shipping: {(shipping === 0) ? "Free!" : "$" + shipping.toFixed(2)}</h4>
                     <h4>Taxes: ${tax.toFixed(2)}</h4>
-                    <h4>Sub-Total: ${subtotal.toFixed(2)}</h4>
+                    <h4>Total: ${total.toFixed(2)}</h4>
                 </div>
                 <Link to='/thankyou'><button onClick={() => clearcart()}>Pay</button></Link>
                 </>
